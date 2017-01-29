4285bd0af9c994b8f974025bbcf60452a08a381f
dddotcom <dddotcom@gmail.com>
2017-01-29 11:36:14 -0800

    modify styling, added contact page

--- main.js
+++ main.js
@@ -18,6 +18,51 @@ $(document).ready(function() {
     return false;
   });
 
+  $("#home-link").click( function() {
+    console.log("homelink clicked");
+    window.location.href = 'alt_index.html';
+    return false;
+  });
+
+  //check that all form inputs are valid
+  // $("#submit-contact").click( function(e) {
+  //   e.preventDefault();
+  //   clearErrors();
+  //   var noErrors = true;
+  //
+  //   if( $("#name-input").val().length === 0 ){
+  //     document.getElementsByClassName('error')[0].textContent = "Please enter your name";
+  //     noErrors = false;
+  //   }
+  //   if( $("#email-input").val().length === 0 ){
+  //     document.getElementsByClassName('error')[1].textContent = "Please enter your email address";
+  //     noErrors = false;
+  //   } else if ( !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test($("#email-input").val())) {
+  //     document.getElementsByClassName('error')[1].textContent = "Please enter a valid email";
+  //     noErrors = false;
+  //   }
+  //
+  //   if( $("#subject-input").val().length === 0 ){
+  //     document.getElementsByClassName('error')[2].textContent = "Please enter your subject";
+  //     noErrors = false;
+  //   }
+  //   if( $("#message-input").val().length === 0 ){
+  //     document.getElementsByClassName('error')[3].textContent = "Pleaese enter your message";
+  //     noErrors = false;
+  //   }
+  //
+  //   if(noErrors) {
+  //     // send email
+  //   }
+  // });
+
+  // function clearErrors() {
+  //   for( var i = 0; i < document.getElementsByClassName('error').length; i++ ) {
+  //     document.getElementsByClassName('error')[i].textContent = "";
+  //   }
+  // }
+
+
   //disable right click on images
   $('img').bind('contextmenu', function(e) {
     return false;
